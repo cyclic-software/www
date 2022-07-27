5d2831406c147ca709a981cca6b6bdd9b49d9206
seekayel <seekayel@users.noreply.github.com>
2022-07-27 20:06:02 +0000

    deploy: d9b22c49a0fecc3a1ce2a6045f03eef5a066d7ab

---
+++ _nuxt/static/1658952361/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1658952361",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"80ba8a4b"}}}}(null,"\u002F"));
