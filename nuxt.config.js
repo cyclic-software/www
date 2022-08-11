e379ee9a16903cd4062a0a05b61366d11f6c4d3c
Kam Lasater <kam.lasater@cyclic.sh>
2022-08-11 15:05:15 -0400

    Reinstall package.json

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,6 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    // title: 'www-builder',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
