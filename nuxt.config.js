d546e735d7d72c2f01525d89b0195e49d648a2cd
Kam Lasater <kam.lasater@cyclic.sh>
2022-08-11 15:49:45 -0400

    Added static serving

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,7 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  ssr: false,
-  // router: {
-  //   base: '/www-builder/'
-  // },
+  // ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
