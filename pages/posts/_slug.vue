eff869d8bf15805480ea52be313ed69954d62b71
Kam Lasater <kam.lasater@cyclic.sh>
2022-08-12 15:41:39 -0400

    First cut at styling post page

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -1,9 +1,105 @@
 <template>
-  <article>
-    <h1>{{ post.title }}</h1>
-    <p>Published: {{ formatDate(post.publishedOn) }}</p>
-    <nuxt-content :document="post" />
-  </article>
+  <div>
+    <div class="section half-padding wf-section">
+      <div class="container w-container">
+        <div class="blog-image-header">
+          <img :src="post.image" alt="" class="image-cover">
+        </div>
+      </div>
+    </div>
+    <div class="section small-padding wf-section">
+      <div data-w-id="cb94c0a0-2bab-b0c9-9da8-d93e4ab951f6" xstyle="opacity:0" class="blog-container w-container">
+        <h1>{{ post.title }}</h1>
+        <div class="byline">
+          <div class="text-block-6">By:</div>
+          <div>
+            <a href="#" class="link-3">{{post.author}}</a>
+            <a href="#" class="link-2">@author-twitter</a>
+          </div>
+          <div class="div-block"></div>
+          <div class="text-block-7">Posted: </div>
+          <div class="text-block-5">Published: {{ formatDate(post.publishedOn) }}</div>
+        </div>
+        <div class="div-block-2">
+          <a href="#" class="link-block w-inline-block w-clearfix">
+            <div class="html-embed share-button w-embed">
+              <a onclick="window.open(this.href, 'twitter-share','width=580,height=296');return false;"
+    class="share-button" target="_blank" href="http://twitter.com/share?url=https://www.cyclic.sh/posts/&text=%20by%20" >
+              </a>
+              <!--
+  Reddit <i class="icon"></i>
+  Envelope <i class="icon"></i>
+  Slack <i class="icon"></i>
+  Hacker News <i class="icon"></i>
+  -->
+            </div>
+            <div class="html-embed share-button w-embed">
+              <a onclick="window.open(this.href, 'linkedin-share','width=530,height=600');return false;"
+    class="share-button" target="_blank" href="https://www.linkedin.com/sharing/share-offsite/?url=https://www.cyclic.sh/posts/">
+              </a>
+              <!--
+  Twitter <i class="icon"></i>
+  Reddit <i class="icon"></i>
+  Envelope <i class="icon"></i>
+  Slack <i class="icon"></i>
+  Hacker News <i class="icon"></i>
+  -->
+            </div>
+          </a>
+        </div>
+        <div class="spacer _32"></div>
+        <div class="rich-text w-richtext">
+          <nuxt-content :document="post"></nuxt-content>
+        </div>
+      </div>
+    </div>
+
+    <!-- <section id="subscribe-form" class="subscribe-form wf-section">
+      <div class="centered-container w-container">
+        <h2>Subscribe to posts</h2>
+        <p>Enter your email If you would like to get an email the next time we post.<br>We post about ~1-3x per month, and up to once a month about company news.<br></p>
+      </div>
+      <div class="container-5 w-container">
+        <div>
+          <div class="w-embed w-script">
+            <script charset="utf-8" type="text/javascript" src="https://js.hsforms.net/forms/shell.js"></script>
+            <script>
+    hbspt.forms.create({
+    region: "na1",
+    portalId: "9213379",
+    formId: "37c53da9-83fc-4c77-af25-644648ce2eb2"
+  });
+  </script>
+          </div>
+        </div>
+      </div>
+    </section> -->
+
+
+    <!-- <div class="section small-padding wf-section">
+      <div class="container w-container">
+        <h2>Related Posts</h2>
+        <div class="spacer _24"></div>
+        <div class="w-dyn-list">
+          <div role="list" class="_12-columns align-stretch w-dyn-items">
+            <div role="listitem" class="column desk-4 w-dyn-item">
+              <a href="#" class="blog-card w-inline-block"><img src="" alt="" class="card-thumbnail">
+                <div class="card-text-container">
+                  <div class="card-tag"></div>
+                  <h4 class="card-title"></h4>
+                  <p class="card-summary"></p>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="w-dyn-empty">
+            <div>No items found.</div>
+          </div>
+        </div>
+      </div>
+    </div> -->
+    <CtaLaunchHelloWorld></CtaLaunchHelloWorld>
+  </div>
 </template>
 
 <script>
