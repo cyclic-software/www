1f1565bdba468a5c0c506c572ce90538e4b1c3b5
seekayel <seekayel@users.noreply.github.com>
2022-08-15 01:53:14 +0000

    deploy: 868b110c63df7445ee9df0b1f6cf67228a2d7ab0

---
+++ _nuxt/static/1660528388/authors/eluda/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660528388",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:null},content:{dbHash:"26caa7ac"}}}}("\u002Fauthors\u002Feluda"));
