53eb89205b9539d5454a872dc893c39ed7be577a
seekayel <seekayel@users.noreply.github.com>
2022-08-15 02:21:12 +0000

    deploy: 530f754e5fb11c4728e81685df0da05177923d2f

---
+++ _nuxt/static/1660530067/posts/users-are-the-best-feature/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660530067",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fusers-are-the-best-feature",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"89b2b998"}}}}(null));
