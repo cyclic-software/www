0ddea6cf8f31c62740c84898988a24065915daf6
seekayel <seekayel@users.noreply.github.com>
2022-08-15 16:02:05 +0000

    deploy: a416d2d5c9b94eb7adc3b4d6e12f74f95f4d8f34

---
+++ _nuxt/static/1660579321/posts/aws-budget-alerts-based-on-cost/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660579321",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Faws-budget-alerts-based-on-cost",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8c3699bc"}}}}(null));
