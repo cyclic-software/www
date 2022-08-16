6807ae1c516a75c606edab541fd10297651afc89
seekayel <seekayel@users.noreply.github.com>
2022-08-16 19:05:29 +0000

    deploy: 34b652c696a7eb52ea792b780d88235a64891a1e

--- _nuxt/static/1660675979/posts/considerations-for-serverless-active-active-routing/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660675979",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fconsiderations-for-serverless-active-active-routing",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bc8731f3"}}}}(null));
---
+++ _nuxt/static/1660676723/posts/we-sound-like-idiots-when-we-talk-about-technical-debt/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660676723",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fwe-sound-like-idiots-when-we-talk-about-technical-debt",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6e3d0316"}}}}(null));
