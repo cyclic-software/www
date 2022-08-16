8c052d251751898bf35ba3855235698af1719ff0
seekayel <seekayel@users.noreply.github.com>
2022-08-16 16:36:13 +0000

    deploy: 4d231c619e4a13227317fc77b9bc058467e33e6d

---
+++ _nuxt/static/1660667767/posts/dads-advice-for-how-to-live/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660667767",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fdads-advice-for-how-to-live",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"22d14fee"}}}}(null));
