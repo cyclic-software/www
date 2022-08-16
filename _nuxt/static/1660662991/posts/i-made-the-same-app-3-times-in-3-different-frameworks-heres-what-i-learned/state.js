703cd4e8f1e009ded5ca00a37333b1a986c033a1
seekayel <seekayel@users.noreply.github.com>
2022-08-16 15:16:37 +0000

    deploy: 9501beca6f5d60a59d4d96794525792dbd8998db

---
+++ _nuxt/static/1660662991/posts/i-made-the-same-app-3-times-in-3-different-frameworks-heres-what-i-learned/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660662991",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fi-made-the-same-app-3-times-in-3-different-frameworks-heres-what-i-learned",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ea5f8232"}}}}(null));
