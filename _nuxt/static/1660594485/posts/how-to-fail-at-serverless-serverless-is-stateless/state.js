695d18ad01abff6a001b8810fe5fc56018d8188b
seekayel <seekayel@users.noreply.github.com>
2022-08-16 00:48:25 +0000

    deploy: 057b71d794e85c2c1ffb0f417c3ba8a44ec85dea

--- _nuxt/static/1660594485/posts/how-to-fail-at-serverless-serverless-is-stateless/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660594485",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fhow-to-fail-at-serverless-serverless-is-stateless",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0bbc30f0"}}}}(null));
