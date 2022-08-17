5cfb6c1b274b9c863920ee551bc621caeb38c8d3
korostelevm <korostelevm@users.noreply.github.com>
2022-08-17 17:33:33 +0000

    deploy: 2fadedbc636bc6861bc13d628aea6a1909b44c66

---
+++ _nuxt/static/1660757607/posts/aws-s3-why-sometimes-you-should-press-the-100k-dollar-button/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660757607",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Faws-s3-why-sometimes-you-should-press-the-100k-dollar-button",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a1734d9e"}}}}(null));
