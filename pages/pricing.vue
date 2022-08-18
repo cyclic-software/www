b883885ae329ec05aa77b9600cf5e526e88ef014
Kam Lasater <kam.lasater@cyclic.sh>
2022-08-18 17:13:58 -0400

    Pricing language tweaks

--- pages/pricing.vue
+++ pages/pricing.vue
@@ -8,39 +8,39 @@
             <p>
               Consumption based pricing lets you start free and pay as your needs and app grows.
             </p>
-            <h3>
-              All plans include:
-            </h3>
-            <ul>
-              <li>Serverless that just works</li>
-              <li>Blazing fast builds</li>
-              <li>Branch based preview env</li>
-              <li>Enterprise grade architecture</li>
-            </ul>
             <div class="spacer _48"></div>
           </div>
           <div class="_12-columns align-stretch align-center">
-            <div class="column desk-3">
+            <div class="column desk-4">
               <div class="pricing-card">
                 <div class="top-block"></div>
-                <h2>Start</h2>
+                <h2>Free Forever</h2>
                 <p>Generous free tier lets you prototype and learn.</p>
                 <div class="spacer _24"></div>
-                <a href="https://app.cyclic.sh/api/login" class="button outline-white w-button">Get Started</a>
+                <a href="https://app.cyclic.sh/api/login" class="button outline-white w-button">Start</a>
                 <div class="spacer _24"></div>
                 <h3 class="no-margin">$0<span class="regular">/month</span></h3>
-                <p class="paragraph-small">up to 100,000 invokes / month</p>
+                <p class="paragraph-small">subject to fair use</p>
+                <!-- <p></p> -->
                 <div class="tick-list w-richtext">
-                  <ul role="list">
-                    <li>Up to 3 apps</li>
-                    <li>API rate limits</li>
+                  <ul>
+                    <li>3 apps</li>
+                    <li>100,000 API requests</li>
+                    <li>1GB runtime memory</li>
+                    <li>512MB /tmp storage</li>
+                    <li>1GB Object Storage (AWS S3)</li>
+                    <li>1GB Database Storage (AWS DynamoDB)</li>
+                    <li>Unlimited blazing fast builds</li>
+                    <li>Public GitHub repositories</li>
+                    <li>Hosted close to you</li>
+                    <li>3 Cron tasks per app</li>
                     <li>7 day log retention</li>
                     <li>Community support</li>
                   </ul>
                 </div>
               </div>
             </div>
-            <div class="column desk-3">
+            <div class="column desk-4">
               <div class="pricing-card">
                 <div class="top-block"></div>
                 <h2>Solo</h2>
@@ -48,43 +48,59 @@
                   Unlock additional developer productivity features.
                 </p>
                 <div class="spacer _24"></div>
-                <a href="https://app.cyclic.sh/api/login" class="button no-margin w-button">Get Started</a>
+                <a href="https://app.cyclic.sh/api/login" class="button no-margin w-button">Upgrade</a>
                 <div class="spacer _24"></div>
                 <h3 class="no-margin">$9<span class="regular"> /month</span></h3>
-                <p class="paragraph-small">minimum against consumption charges</p>
+                <p class="paragraph-small">
+                  plus consumption charges beyond quota
+                </p>
+                <p>All of Free plus:</p>
                 <div class="tick-list w-richtext">
-                  <ul role="list">
+                  <ul>
+                    <li>10 apps</li>
+                    <li>Private GitHub repositories</li>
+                    <li>10 Cron tasks per app</li>
                     <li>Search structured logs</li>
-                    <li>Private repositories</li>
-                    <li>Log retention 1 month</li>
+                    <li>Branch based preview envs</li>
+                    <li>30 day log retention</li>
                     <li>Email support</li>
                   </ul>
                 </div>
               </div>
             </div>
-            <div class="column desk-3">
+            <div class="column desk-4">
               <div class="pricing-card blue">
                 <div class="top-block blue"></div>
                 <h2>Grow</h2>
                 <p>
-                  Take your apps to the next level.
+                  Take your company and apps to the next level.
                 </p>
                 <div class="spacer _24"></div>
-                <a href="https://app.cyclic.sh/api/login" class="button no-margin w-button">Get Started</a>
+                <a href="https://app.cyclic.sh/api/login" class="button no-margin w-button">Buy</a>
                 <div class="spacer _24"></div>
                 <h3 class="no-margin">$199<span class="regular"> /month</span></h3>
                 <p class="paragraph-small"><br></p>
+                <p>All of Solo plus:</p>
                 <div class="tick-list w-richtext">
-                  <ul role="list">
+                  <ul>
+                    <li>Unlimited apps</li>
                     <li>Organization GitHub accounts</li>
-                    <li>Private repositories</li>
-                    <li>Encrypted environment variables.</li>
-                    <li>Email support</li>
+                    <li>Unlimited Cron tasks</li>
+                    <li>90 day log retention</li>
+                    <li>Chat support</li>
                   </ul>
                 </div>
               </div>
             </div>
-            <div class="column desk-3">
+          </div>
+        </div>
+      </div>
+
+
+      <div class="section">
+        <div class="container center-align w-container">
+          <div class="_12-columns align-stretch align-center"></div>
+            <div class="column desk-12">
               <div class="pricing-card dark-blue">
                 <h2>Scale</h2>
                 <p>Data governance, Security, Compliance and Auditing handled.</p>
@@ -94,12 +110,12 @@
                 <h3 class="no-margin">Based on requirements<span class="regular"></span></h3>
                 <p class="paragraph-small"><br></p>
                 <div class="tick-list w-richtext">
-                  <ul role="list">
+                  <ul>
                     <li>BYO Cloud Account</li>
                     <li>BYO IdP / SSO</li>
                     <li>SOC2/PCI/HIPAA</li>
                     <li>Dedicated relationship manager</li>
-                    <li>Phone support</li>
+                    <li>Architectural reviews</li>
                   </ul>
                 </div>
                 <div class="top-block dark-blue"></div>
@@ -108,153 +124,6 @@
           </div>
         </div>
       </div>
-
-
-
-      <!-- <div class="section no-padding mobile-hidden">
-        <div class="spacer _80"></div>
-        <div class="container">
-          <div class="pricing-grid">
-            <div class="pricing-grid-row top">
-              <div class="pricing-feature-name">
-                <h2 class="no-margin">Plan breakdown</h2>
-                <p>Find the plan that&#x27;s right for you.</p>
-              </div>
-              <div class="plan-header">
-                <h3 class="no-margin">Start</h3>
-                <p class="paragraph-small"><strong>$19</strong><span>/month</span></p>
-                <a href="sign-up.html" class="small-button secondary w-button">Sign Up</a>
-              </div>
-              <div class="plan-header green">
-                <h3 class="no-margin">Grow</h3>
-                <p class="paragraph-small"><strong>$49</strong><span>/month</span></p>
-                <a href="sign-up.html" class="small-button w-button">Sign Up</a>
-              </div>
-              <div class="plan-header last">
-                <h3 class="no-margin">Scale</h3>
-                <p class="paragraph-small"><strong>$79</strong><span>/month</span></p>
-                <a href="sign-up.html" class="small-button secondary w-button">Sign Up</a>
-              </div>
-            </div>
-            <div class="pricing-grid-section">
-              <h3>Usage</h3>
-              <div class="pricing-grid-row">
-                <p>Pages and blocks</p>
-                <p class="medium-paragraph">Up to 50/month</p>
-                <p class="medium-paragraph">Up to 200/month</p>
-                <p class="medium-paragraph">Unlimited</p>
-              </div>
-              <div class="pricing-grid-row">
-                <p>Team members</p>
-                <p class="medium-paragraph">Up to 3</p>
-                <p class="medium-paragraph">Up to 12</p>
-                <p class="medium-paragraph">Unlimited</p>
-              </div>
-              <div class="pricing-grid-row">
-                <p>Version history</p><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick hidden"><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick"><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick">
-              </div>
-            </div>
-            <div class="pricing-grid-section">
-              <h3>Features</h3>
-              <div class="pricing-grid-row">
-                <p>Priority support</p><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick hidden"><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick"><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick">
-              </div>
-              <div class="pricing-grid-row">
-                <p>Wikis, docs, &amp; notes<br></p><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick"><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick"><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick">
-              </div>
-              <div class="pricing-grid-row">
-                <p>50+ starter templates</p><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick hidden"><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick hidden"><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick">
-              </div>
-              <div class="pricing-grid-row">
-                <p>Version history</p><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick"><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick"><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick">
-              </div>
-              <div class="pricing-grid-row">
-                <p>Permission groups</p><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick"><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick"><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick">
-              </div>
-              <div class="pricing-grid-row">
-                <p>Unlimited team members</p><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick"><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick"><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick">
-              </div>
-              <div class="pricing-grid-row">
-                <p>Custom contract &amp; invoicing</p><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick hidden"><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick hidden"><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick">
-              </div>
-              <div class="pricing-grid-row">
-                <p>Web, Desktop, iOS, and Android apps</p><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick hidden"><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick"><img src="images/Blue-Tick.svg" loading="lazy" width="20" alt="" class="pricing-grid-tick">
-              </div>
-              <div class="pricing-grid-row">
-                <p>Advanced security controls</p>
-                <p class="medium-paragraph">None</p>
-                <p class="medium-paragraph">Unlimited</p>
-                <p class="medium-paragraph">Unlimited</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="section no-padding">
-        <div class="spacer _80"></div>
-        <div class="container">
-          <div class="_12-columns align-center">
-            <div class="column desk-3 tab-2 mob-0"></div>
-            <div class="column desk-6 tab-8 mob-12">
-              <h2>Got any questions? We have the answers.</h2>
-              <div class="spacer _32"></div>
-              <div class="questions">
-                <a href="#" data-ix="show-answer" class="question-container w-inline-block">
-                  <div class="question">
-                    <h4>What happens if I run out of credit?</h4><img src="images/Dropdown-Arrow.svg" alt="" class="question-arrow-icon">
-                  </div>
-                  <div data-ix="hide-on-load" style="display:none;height:0PX" class="answer">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                  </div>
-                </a>
-                <a href="#" data-ix="show-answer" class="question-container w-inline-block">
-                  <div class="question">
-                    <h4>How is pricing calculated?</h4><img src="images/Dropdown-Arrow.svg" alt="" class="question-arrow-icon">
-                  </div>
-                  <div data-ix="hide-on-load" style="display:none;height:0PX" class="answer">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                  </div>
-                </a>
-                <a href="#" data-ix="show-answer" class="question-container w-inline-block">
-                  <div class="question">
-                    <h4>How does adding members work?</h4><img src="images/Dropdown-Arrow.svg" alt="" class="question-arrow-icon">
-                  </div>
-                  <div data-ix="hide-on-load" style="display:none;height:0PX" class="answer">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                  </div>
-                </a>
-                <a href="#" data-ix="show-answer" class="question-container w-inline-block">
-                  <div class="question">
-                    <h4>What happens when I change plans?</h4><img src="images/Dropdown-Arrow.svg" alt="" class="question-arrow-icon">
-                  </div>
-                  <div data-ix="hide-on-load" style="display:none;height:0PX" class="answer">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                  </div>
-                </a>
-                <a href="#" data-ix="show-answer" class="question-container w-inline-block">
-                  <div class="question">
-                    <h4>Can I change my payment method?</h4><img src="images/Dropdown-Arrow.svg" alt="" class="question-arrow-icon">
-                  </div>
-                  <div data-ix="hide-on-load" style="display:none;height:0PX" class="answer">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-                  </div>
-                </a>
-                <a href="#" data-ix="show-answer" class="question-container w-inline-block">
-                  <div class="question">
-                    <h4>How does removing members work?</h4><img src="images/Dropdown-Arrow.svg" alt="" class="question-arrow-icon">
-                  </div>
-                  <div data-ix="hide-on-load" style="display:none;height:0PX" class="answer">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                  </div>
-                </a>
-              </div>
-            </div>
-            <div class="column desk-3 tab-2 mob-0"></div>
-          </div>
-        </div>
-        <div class="spacer _80"></div>
-      </div> -->
-
 
       <div class="spacer _64"></div>
 
