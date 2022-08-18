7c0b49df3511ef61c5cf24058b6db0d117c8314c
seekayel <seekayel@users.noreply.github.com>
2022-08-18 21:20:56 +0000

    deploy: a602990bb22636f5ad5f5b1a0a72e9239e11d8ca

---
+++ _nuxt/static/1660857650/posts/considerations-for-serverless-active-active-routing/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660857650",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fconsiderations-for-serverless-active-active-routing",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"65ea00d7"}}}}(null));
