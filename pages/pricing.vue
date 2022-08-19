380e6b37121c7d1be9d02b107139b820f07585b8
Kam Lasater <kam.lasater@cyclic.sh>
2022-08-19 12:22:30 -0400

    added stripe buy links

--- pages/pricing.vue
+++ pages/pricing.vue
@@ -48,7 +48,7 @@
                   Unlock additional developer productivity features.
                 </p>
                 <div class="spacer _24"></div>
-                <a href="https://app.cyclic.sh/api/login" class="button no-margin w-button">Upgrade</a>
+                <a href="https://buy.stripe.com/dR6g091JE2e45XObII" target="_blank" class="button no-margin w-button">Upgrade</a>
                 <div class="spacer _24"></div>
                 <h3 class="no-margin">$9<span class="regular"> /month</span></h3>
                 <p class="paragraph-small">
@@ -77,7 +77,7 @@
                   Take your company and apps to the next level.
                 </p>
                 <div class="spacer _24"></div>
-                <a href="https://app.cyclic.sh/api/login" class="button no-margin w-button">Buy</a>
+                <a href="https://buy.stripe.com/3cs01b0FAaKAfyo7st" target="_blank" class="button no-margin w-button">Buy</a>
                 <div class="spacer _24"></div>
                 <h3 class="no-margin">$299<span class="regular"> /month</span></h3>
                 <p class="paragraph-small"><br></p>
