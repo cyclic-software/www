b924210e877b5299f85ddc72c8930e06f5809870
seekayel <seekayel@users.noreply.github.com>
2022-08-22 21:03:46 +0000

    deploy: 2961743c2c4fafcf0fd2325d80d12a9035b2365a

--- _nuxt/static/1661194660/posts/angellist-roll-up-vehicle-why-what-and-how/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1661194660",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fangellist-roll-up-vehicle-why-what-and-how",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5642bf4e"}}}}(null));
