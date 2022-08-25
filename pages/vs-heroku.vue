19a5ae16343722164f169d016f5e8fd4be0c7e2c
Kam Lasater <kam.lasater@cyclic.sh>
2022-08-24 21:38:03 -0400

    Add spacers

--- pages/vs-heroku.vue
+++ pages/vs-heroku.vue
@@ -9,8 +9,11 @@
     </p>
   </div>
 
-  <h2>Compute</h2>
   <b-container class="feature-matrix">
+    <b-row class="header-row">
+      <b-col cols="6">Compute</b-col>
+      <b-col>Cyclic</b-col> <b-col>Heroku</b-col>
+    </b-row>
     <b-row>
       <b-col cols="6">Always on</b-col>
       <b-col>✔︎</b-col><b-col>Paid</b-col>
@@ -29,10 +32,11 @@
     </b-row>
   </b-container>
 
-  <h2>Storage</h2>
+  <div class="spacer-64"></div>
+
   <b-container class="feature-matrix">
     <b-row class="header-row">
-      <b-col cols="6"></b-col>
+      <b-col cols="6">Storage</b-col>
       <b-col>Cyclic</b-col> <b-col>Heroku</b-col>
     </b-row>
     <b-row>
@@ -45,10 +49,11 @@
     </b-row>
   </b-container>
 
-  <h2>DevX</h2>
+  <div class="spacer-64"></div>
+
   <b-container class="feature-matrix">
     <b-row class="header-row">
-      <b-col cols="6"></b-col>
+      <b-col cols="6">DevX</b-col>
       <b-col>Cyclic</b-col><b-col>Heroku</b-col>
     </b-row>
     <b-row>
@@ -59,12 +64,17 @@
       <b-col cols="6">GitHub app integration</b-col>
       <b-col>✔︎</b-col><b-col>✔︎</b-col>
     </b-row>
+    <b-row>
+      <b-col cols="6">Zero-config</b-col>
+      <b-col>✔︎</b-col><b-col>x</b-col>
+    </b-row>
   </b-container>
 
-  <h2>Pricing</h2>
+  <div class="spacer-64"></div>
+
   <b-container class="feature-matrix">
     <b-row class="header-row">
-      <b-col cols="6"></b-col>
+      <b-col cols="6">Pricing</b-col>
       <b-col>Cyclic</b-col> <b-col>Heroku</b-col>
     </b-row>
     <b-row>
@@ -73,17 +83,16 @@
     </b-row>
   </b-container>
 
+  <div class="spacer-64"></div>
+
   <div class="section">
-    <h2>Where Cyclic does better:</h2>
     <p>
       Cyclic shines due to a modern cloud architecture and serverless design. If your service load is bursty or
       if you need to self host.
     </p>
     <ul>
-      <li>No sleep - 10x-20x faster first requests (sub-1s vs 30s)</li>
       <li>No complicated cli: use the tools you know and love</li>
       <li>Zero Config - auto build, deploy and launch</li>
-      <li>Self host in your own AWS account</li>
       <li>Eject at anytime</li>
       <li>No technical lock-in</li>
       <li>No data lock-in</li>
@@ -96,8 +105,20 @@
 
 <style scoped>
 
-.feature-matrix b-col {
+.feature-matrix {
   border: 1px;
 }
 
+.spacer-32 {
+  min-height: 32px;
+  max-height: 32px;
+  min-width: 32px;
+  max-width: 32px;
+}
+.spacer-64 {
+  min-height: 64px;
+  max-height: 64px;
+  min-width: 64px;
+  max-width: 64px;
+}
 </style>
