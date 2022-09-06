574467eb71ed48ba69ec53dba9fee43fa5d7ac01
korostelevm <korostelevm@users.noreply.github.com>
2022-09-06 15:43:25 +0000

    deploy: f7e38b9d22ee869d07dfaad9a4eee2d69d225028

--- _nuxt/static/1661954981/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1661954981",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"676372ec"}}}}(null,"\u002F"));
