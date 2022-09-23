d4117bc4ccdbb85bfb07c5a5cd0cb2418fe30fd7
Mike <korostelevm@gmail.com>
2022-09-23 09:26:25 -0400

    full width blue

--- components/Menu.vue
+++ components/Menu.vue
@@ -13,31 +13,34 @@
       <a href="https://app.cyclic.sh/api/login" class="nav-item nav-link" style="float: right;">Sign In</a>
     </div>
   </nav> -->
+<div>
+  <div id="menubar">
+    <b-navbar id="menu" toggleable="md" type="dark" variant="none">
+      <!-- <div id="menu-content"> -->
+        <b-navbar-brand to="/" class="navbar-brand">
+          <img src="/images/cyclic-logo.png" class="logo-img"/>
+        </b-navbar-brand>
 
-<div id="menubar">
-  <b-navbar id="menu" toggleable="md" type="dark" variant="none" fixed="top">
-    <b-navbar-brand to="/" class="navbar-brand">
-      <img src="/images/cyclic-logo.png" class="logo-img"/>
-    </b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse" float="right"></b-navbar-toggle>
 
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item to="/pricing">Pricing</b-nav-item>
+            <b-nav-item to="/blog">Blog</b-nav-item>
+            <b-nav-item href="https://docs.cyclic.sh/" target="_blank">Docs</b-nav-item>
+            <b-nav-item to="/vs-heroku">vs Heroku</b-nav-item>
+          </b-navbar-nav>
 
-    <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav>
-        <b-nav-item to="/pricing">Pricing</b-nav-item>
-        <b-nav-item to="/blog">Blog</b-nav-item>
-        <b-nav-item href="https://docs.cyclic.sh/" target="_blank">Docs</b-nav-item>
-        <b-nav-item to="/vs-heroku">vs Heroku</b-nav-item>
-      </b-navbar-nav>
-
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-        <b-nav-item href="https://app.cyclic.sh/api/login">
-          Sign In
-        </b-nav-item>
-      </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item href="https://app.cyclic.sh/api/login">
+              Sign In
+            </b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      <!-- </div> -->
+    </b-navbar>
+  </div>
   <div class="sp-65"></div>
 </div>
 
@@ -54,68 +57,41 @@
   }
   .navbar-brand {
     min-width: 85px;
-    /* min-height: 55px; */
   }
-  #nav-collapse .show {
+  /* #nav-collapse .show {
     padding-left: 120px;
-  }
+  } */
   #menu {
     min-height: 65px;
     margin-right: auto;
     margin-left: auto;
     max-width: 1200px;
+    width: 100%;
+    /* display:flex; */
   }
   #menubar {
-    position: relative;
-    left: 0px;
+    z-index:100;
+    position: fixed;
+    /* left: 0px;
     top: 0px;
     right: 0px;
-    display: block;
+    display: block; */
     width: 100%;
-    max-width: 1230px;
-    margin-right: auto;
-    margin-left: auto;
+    /* max-width: 1230px; */
+    /* margin-right: auto;
+    margin-left: auto; */
     padding-right: 40px;
     padding-left: 40px;
-    flex-direction: column;
-    justify-content: center;
+    /* flex-direction: column;
+    justify-content: center; */
   }
 
-
-/* // Medium devices (tablets, 768px and up)
-@media (min-width: 768px) { ... }
-// Extra large devices (large desktops, 1200px and up)
-@media (min-width: 1200px) { ... } */
-
-  /* @media (min-width: 576px) {
-    #menu {
-      padding-left: 40px;
-      padding-right: 40px;
-    }
-  }
-  @media (min-width: 992px) {
-    #menu {
-      padding-left: 80px;
-      padding-right: 80px;
-    }
-  } */
   .sp-65 {
     min-height: 65px;
   }
-  /* li.nav-item {
-    margin-bottom: 0px
-  } */
-  /* .navbar {
-    position: -webkit-sticky;
-    position: sticky;
-    left: 0px;
-    top: 0px;
-    right: 0px;
-    z-index: 200;
-  } */
   .logo-img {
-    position: fixed;
-    top: 5px;
+    /* position: fixed;
+    top: 5px; */
     height: 55px
   }
   .nav-item{
@@ -132,7 +108,7 @@
   export default {
     methods: {
       handleScroll() {
-        document.getElementById('menu').style['background-color'] = scale(window.pageYOffset);
+        document.getElementById('menubar').style['background-color'] = scale(window.pageYOffset);
       }
     },
     beforeMount () {
