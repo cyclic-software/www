6daee931f35ead39f27559af79280bc3632b62ae
Mike <korostelevm@gmail.com>
2022-11-04 15:30:40 -0400

    mobile

--- mixins/global.js
+++ mixins/global.js
@@ -10,15 +10,15 @@ export default {
     },
     methods:{
         async ga_event(event_name, callback){
-            try{
-                  await gtag('event', event_name, {
-                  'event_label' : window.location.href,
-                  'event_category' : event_name,
-                  'event_callback': callback
-                  });  
-              }catch(e){ 
-                 callback()
-              }
+            // try{
+            //       await gtag('event', event_name, {
+            //       'event_label' : window.location.href,
+            //       'event_category' : event_name,
+            //       'event_callback': callback
+            //       });  
+            //   }catch(e){ 
+            //      callback()
+            //   }
         }
       }
   }
