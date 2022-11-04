c69b7bf262e91a96a49017fa26ebf6d940da491e
korostelevm <korostelevm@users.noreply.github.com>
2022-11-04 14:13:19 +0000

    deploy: a833a41efbb9f8247dd1344d20043834d02911b0

--- _nuxt/static/1667402140/privacy-policy/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1667402140",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprivacy-policy",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0db6fe55"}}}}(null));
