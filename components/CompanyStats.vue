29ea394422a93d8a5da7482cbfbce511da7529e2
Kam Lasater <kam.lasater@cyclic.sh>
2022-11-04 10:10:24 -0400

    stats as of 20221104

--- components/CompanyStats.vue
+++ components/CompanyStats.vue
@@ -13,21 +13,21 @@
         </div> -->
         <div class="column desk-4">
           <div class="text-box">
-            <h2 class="header-large">1,960+</h2>
+            <h2 class="header-large">2,140+</h2>
             <p>Developers active this week</p>
           </div>
           <div class="spacer _32"></div>
         </div>
         <div class="column desk-4">
           <div class="text-box">
-            <h2 class="header-large">11,200+</h2>
+            <h2 class="header-large">11,480+</h2>
             <p>Developers on Cyclic</p>
           </div>
           <div class="spacer _32"></div>
         </div>
         <div class="column desk-4">
           <div class="text-box">
-            <h2 class="header-large">128,900+</h2>
+            <h2 class="header-large">133,800+</h2>
             <p>Builds triggered</p>
           </div>
           <div class="spacer _32"></div>
