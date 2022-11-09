d8741f86ca535dff82a1439950d20695e777c287
seekayel <seekayel@users.noreply.github.com>
2022-11-09 18:51:24 +0000

    deploy: b21b1c0a93e31721a08b23bcd832d54607d58a06

---
+++ _nuxt/static/1668019866/posts/the-vc-business-model-and-why-vcs-invest/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1668019866",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fthe-vc-business-model-and-why-vcs-invest",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c3356634"}}}}(null));
