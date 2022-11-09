2ec1170d39eca7364001dfbd95f90610a8bcac75
Mike <korostelevm@gmail.com>
2022-11-09 16:12:39 -0500

    stats

--- components/CompanyStats.vue
+++ components/CompanyStats.vue
@@ -14,21 +14,21 @@
         </div> -->
         <div class="column desk-4">
           <div class="text-box">
-            <h2 class="header-large">2,140+</h2>
+            <h2 class="header-large">2,160+</h2>
             <p>Developers active this week</p>
           </div>
           <div class="spacer _32"></div>
         </div>
         <div class="column desk-4">
           <div class="text-box">
-            <h2 class="header-large">11,480+</h2>
+            <h2 class="header-large">12,440+</h2>
             <p>Developers on Cyclic</p>
           </div>
           <div class="spacer _32"></div>
         </div>
         <div class="column desk-4">
           <div class="text-box">
-            <h2 class="header-large">133,800+</h2>
+            <h2 class="header-large">151,600+</h2>
             <p>Builds triggered</p>
           </div>
           <div class="spacer _32"></div>
