6bd57bd8bffda6e270271c1250f676912a96720c
Mike <korostelevm@gmail.com>
2022-11-09 13:17:21 -0500

    fix hero animation

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div class="header_image fade_loop main_splash"></div>
     <div class="home_content">
       <div class="front-page-spacer"></div>
-      <heroZ/>
+      <hero/>
 
       <div class="front-page-spacer "></div>
       <frameworks/>
