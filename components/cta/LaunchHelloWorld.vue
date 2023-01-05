af965f2f0da8d949c0d17f85263fe4518086eecf
Bioga Allen <allenojuka10@gmail.com>
2023-01-05 19:41:18 +0300

    Fix font size and vertical spacing.

--- components/cta/LaunchHelloWorld.vue
+++ components/cta/LaunchHelloWorld.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="banner wf-section">
-    <div class="container w-container">
+    <div class="container w-container max-w-screen-xl">
       <div class="_12-columns align-center">
         <div class="column desk-12"><img src="/images/Rectangle-240-copy.svg" loading="lazy" alt="" class="absolute-image">
           <div class="cta-box">
-            <div class="text-box _600px">
-              <h2>Launch a hello world</h2>
-              <p>Fork and deploy a hello world project onto Cyclic faster than it has taken to read this page. Experience the power.</p>
+            <div class="text-box _600px space-y-3">
+              <h2 class="text-4xl font-bold">Launch a hello world</h2>
+              <p class="text-lg">Fork and deploy a hello world project onto Cyclic faster than it has taken to read this page. Experience the power.</p>
               <div class="spacer _24"></div>
               <a href="https://app.cyclic.sh/api/login"
               @click="sign_up('https://app.cyclic.sh/api/login?utm_source=www&utm_medium=web&utm_campaign=cta', $event)" 
