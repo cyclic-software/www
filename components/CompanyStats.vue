83627711984144c6649ccbbaaec353f879cd6720
Kam Lasater <kam.lasater@cyclic.sh>
2023-01-10 06:27:59 -0500

    fix: bump stats for jan 9 23

--- components/CompanyStats.vue
+++ components/CompanyStats.vue
@@ -14,21 +14,21 @@
         </div> -->
         <div class="column desk-4">
           <div class="text-box space-y-1">
-            <h2 class="header-large">4,260+</h2>
+            <h2 class="header-large">3,860+</h2>
             <p class="text-lg">Developers active this week</p>
           </div>
           <div class="spacer _32"></div>
         </div>
         <div class="column desk-4">
           <div class="text-box space-y-1">
-            <h2 class="header-large">25,860+</h2>
+            <h2 class="header-large">30,560+</h2>
             <p class="text-lg">Developers on Cyclic</p>
           </div>
           <div class="spacer _32"></div>
         </div>
         <div class="column desk-4">
           <div class="text-box space-y-1">
-            <h2 class="header-large">273,600+</h2>
+            <h2 class="header-large">322,200+</h2>
             <p class="text-lg">Builds triggered</p>
           </div>
           <div class="spacer _32"></div>
