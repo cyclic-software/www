f230de28a97412656e9ece4099e11fbf8cbd1fe6
korostelevm <korostelevm@users.noreply.github.com>
2023-01-26 16:03:34 +0000

    deploy: 81d0c303c9052a1cd531f5278d030a653d2cdf25

--- _nuxt/static/1674672107/posts/considerations-for-serverless-active-active-routing/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1674672107",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fconsiderations-for-serverless-active-active-routing",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b5b059d5"}},globalRefs:{}}}(null));
---
+++ _nuxt/static/1674748995/posts/aws-budget-alerts-based-on-cost/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1674748995",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Faws-budget-alerts-based-on-cost",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"069dbb8c"}},globalRefs:{}}}(null));
