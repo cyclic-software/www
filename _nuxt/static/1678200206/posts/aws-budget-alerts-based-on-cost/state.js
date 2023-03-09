f81bb9f1f0c6c52126687fd44d2587ead1867776
seekayel <seekayel@users.noreply.github.com>
2023-03-09 14:46:14 +0000

    deploy: c7faefd467b9fc5bbcd91b5ead7ea301b5900940

--- _nuxt/static/1678200206/posts/aws-budget-alerts-based-on-cost/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1678200206",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Faws-budget-alerts-based-on-cost",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6e85d265"}},globalRefs:{}}}(null));
---
+++ _nuxt/static/1678373154/posts/technical-debt-a-field-guide/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1678373154",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Ftechnical-debt-a-field-guide",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"db35a9cf"}},globalRefs:{}}}(null));
