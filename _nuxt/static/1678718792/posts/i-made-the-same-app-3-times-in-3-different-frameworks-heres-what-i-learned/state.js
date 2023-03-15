b40cdef67a396c8ae6905b7c134446637471ab84
korostelevm <korostelevm@users.noreply.github.com>
2023-03-15 14:51:20 +0000

    deploy: 24e32f05390b423b1d9f724ac4367784b2533ffe

--- _nuxt/static/1678718792/posts/i-made-the-same-app-3-times-in-3-different-frameworks-heres-what-i-learned/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1678718792",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fi-made-the-same-app-3-times-in-3-different-frameworks-heres-what-i-learned",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"20544913"}},globalRefs:{}}}(null));
