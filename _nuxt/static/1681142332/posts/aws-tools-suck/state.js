4a6ba5de6cd58210aeee273105bdc60abcc4964b
korostelevm <korostelevm@users.noreply.github.com>
2023-04-26 21:18:15 +0000

    deploy: b53981c167c727a214226627520cd0f14fd37c6e

--- _nuxt/static/1681142332/posts/aws-tools-suck/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1681142332",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Faws-tools-suck",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9dfe64e8"}},globalRefs:{}}}(null));
