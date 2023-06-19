74d1a9d1c2abaa6c66b039e6391376afbaae6704
seekayel <seekayel@users.noreply.github.com>
2023-06-19 19:35:28 +0000

    deploy: 9e7b1d9379159eb2a7ecad4f5d8b4a717b492e1f

--- _nuxt/static/1686928595/posts/write-shitty-code/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1686928595",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fwrite-shitty-code",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"230b1cea"}},globalRefs:{}}}(null));
