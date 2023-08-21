51991ca3a1242553904236e3a7c201b471c715d0
laweyez <allenojuka10@gmail.com>
2023-08-21 23:46:12 +0300

    Add styles.

--- components/Pill.vue
+++ components/Pill.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="border !border-neutral-700 rounded-full px-3 py-1.5 text-sky-500 text-sm
-  font-medium">
+  font-medium w-max text-center">
     {{ text }}
   </div>
 </template>
