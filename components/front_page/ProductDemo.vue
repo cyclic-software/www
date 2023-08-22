3e4c428dbaf1cdd8aaae5f13bcb400162e661d26
laweyez <allenojuka10@gmail.com>
2023-08-22 20:16:56 +0300

    Updates.

--- components/front_page/ProductDemo.vue
+++ components/front_page/ProductDemo.vue
@@ -1,16 +1,23 @@
 <template>
-    <b-container>
-      <b-row>
-            <b-col sm="6" class="text-center" align-self="center">
-              <h1 class="header-large  m-5" style="font-size:45px;">
-      Watch a product tour
-    </h1>
+  <section class="section common-section">
+    <!-- ********* PILL TEXT ********* -->
+    <Pill text="Still not convinced?" />
+    
+    <!-- ********* CTA TEXTS ********* -->
+    <div class="space-y-6">
+      <h2 class="font-extrabold text-4xl lg:text-5xl text-center">
+        Watch our product tour
+      </h2>
+  
+      <p class="text-center text-neutral-300">
+        Join our cool C.E.O, Kam, for a quick tour of Cyclic.
+      </p>
+    </div>
 
-            </b-col>
-            <b-col class=" text-center" sm="6" align-self="center">
-              <iframe
-        width="100%"
-        height="315"
+    <!-- ********* CONTENT ********* -->
+    <div class="w-full mt-10">
+      <iframe
+        class="aspect-auto w-full lg:w-[90%] mx-auto h-[400px] rounded-2xl blue-shadow"
         src="https://www.youtube-nocookie.com/embed/UnhTGEtOD6M"
         title="YouTube video player"
         frameborder="0"
@@ -20,12 +27,8 @@
         srcdoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href='https://www.youtube-nocookie.com/embed/UnhTGEtOD6M?autoplay=1'><img src='https://i.ytimg.com/vi/iK3ael3pQw8/hqdefault.jpg' alt='Cyclic - Full stack serverless hosting platform'><span>▶</span></a>"
       >
       </iframe>
-
-      
-            </b-col>
-        </b-row>
-    </b-container>
-
+    </div>
+  </section>
 </template>
 
 <style scoped>
