dfab4deb7d042bd4420d6e6691b9655688dd036f
laweyez <allenojuka10@gmail.com>
2023-08-22 21:19:48 +0300

    Update updates.

--- pages/updates.vue
+++ pages/updates.vue
@@ -1,55 +1,60 @@
 <template>
-  <div class="w-full space-y-10 gradient-bg">
-    <div class="px-10 py-20 max-w-screen-xl mx-auto space-y-10">
-      <!--************ HEADER ************-->
-      <div class="space-y-4 pb-10 border-b border-white/20">
-        <h1 class="text-7xl font-black capitalize m-0 mb-4 tracking-tight">
+  <section class="section space-y-12">
+    <div class="common-section">
+      <!-- ********* PILL TEXT ********* -->
+      <Pill text="Don't miss a thing" />
+      
+      <!-- ********* CTA TEXTS ********* -->
+      <div class="space-y-6">
+        <h2 class="font-extrabold text-4xl lg:text-5xl text-center capitalize">
           product updates
-        </h1>
-
-        <p class="text-neutral-300 text-lg">
+        </h2>
+    
+        <p class="text-center text-neutral-300">
           Stay up to date with the latest product updates.
         </p>
       </div>
-      
-      <!--************ CONTENT ************-->
-      <div class="grid grid-cols-1 xl:grid-cols-4 gap-8">
-        <!--************ SIDE ************-->
-        <div class="col-span-1 sticky top-24 h-max py-2 px-4 border
-        !border-neutral-800 bg-black/95 rounded-full xl:!border-0 xl:!p-0
-        xl:bg-transparent">
-          <span class="font-semibold capitalize">all</span>
-        </div>
-  
-        <!--************ UPDATES ************-->
-        <div class="col-span-1 xl:col-span-3 space-y-10">
-          <div class="text-sm" v-if="loading">Loading updates...</div>
-          <div
-            v-for="update of updates"
-            :key="update.key"
-            class="space-y-6 border-b border-white/10 pb-6"
-          >
-            <div class="space-y-2">
-              <div class="pb-2">
-                <img
-                  v-if="update.props.image"
-                  :src="update.props.image"
-                  :alt="update.props.title"
-                  class="w-96 h-auto rounded-xl"
-                >
-              </div>
-              <h3 class="text-3xl font-semibold">{{ update.props.title }}</h3>
-              <p class="text-sky-500 text-sm">
-                {{ new Date(update.props.published).toDateString() }}
-              </p>
-            </div>
+    </div>
 
-            <div class="prose prose-lg prose-invert" v-html="$md.render(update.props.content || '')"></div>
+    <hr class="border-neutral-800">
+      
+    <!--************ CONTENT ************-->
+    <div class="grid grid-cols-1 xl:grid-cols-4 gap-8">
+      <!--************ SIDE ************-->
+      <div class="col-span-1 sticky top-24 h-max py-2 px-4 border
+      !border-neutral-700 bg-neutral-800/10 rounded-full xl:!border-0 xl:!p-0
+      xl:bg-transparent">
+        <span class="font-semibold capitalize">all</span>
+      </div>
+
+      <!--************ UPDATES ************-->
+      <div class="col-span-1 xl:col-span-3 space-y-10">
+        <div class="text-sm" v-if="loading">Loading updates...</div>
+        <div
+          v-for="update of updates"
+          :key="update.key"
+          class="space-y-6 border-b border-white/10 pb-6"
+        >
+          <div class="space-y-2">
+            <div class="pb-2">
+              <img
+                v-if="update.props.image"
+                :src="update.props.image"
+                :alt="update.props.title"
+                class="w-96 h-auto rounded-xl"
+              >
+            </div>
+            <h3 class="text-3xl font-semibold">{{ update.props.title }}</h3>
+            <p class="text-sky-500 text-sm">
+              {{ new Date(update.props.published).toDateString() }}
+            </p>
           </div>
+
+          <div class="prose prose-lg prose-invert" v-html="$md.render(update.props.content || '')"></div>
         </div>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
