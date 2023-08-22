6e6afc3f49e5f7dd0deae244841b423782f0ece3
laweyez <allenojuka10@gmail.com>
2023-08-22 19:25:12 +0300

    Add infrastructure section.

---
+++ components/front_page/Infrastructure.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="section common-section">
+    <!-- ********* PILL TEXT ********* -->
+    <Pill text="Behind the scenes" />
+    
+    <!-- ********* CTA TEXTS ********* -->
+    <div class="space-y-6">
+      <h2 class="font-extrabold text-4xl lg:text-5xl text-center">
+        AWS-backed infrastructure
+      </h2>
+  
+      <p class="text-center text-neutral-300">
+        Let us handle everything or bring your own AWS account.
+      </p>
+    </div>
+
+    <!-- ********* CONTENT ********* -->
+    <div class=""></div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'Infrastructure'
+  }
+</script>
