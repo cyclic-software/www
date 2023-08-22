d17611a2b81834ef183473de6fe6cdae28f0faef
seekayel <seekayel@users.noreply.github.com>
2023-08-22 20:17:25 +0000

    deploy: f7a8aa90873d2eee4641e75ffe439ec6f5b3ed9d

--- _nuxt/static/1690478320/posts/how-to-pass-9-aws-certifications-in-14-months/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1690478320",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fhow-to-pass-9-aws-certifications-in-14-months",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1d6be991"}},globalRefs:{}}}(null));
