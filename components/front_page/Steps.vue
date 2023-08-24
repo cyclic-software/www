fc75987250614a1dc20cd38e29b62361989a0006
laweyez <allenojuka10@gmail.com>
2023-08-24 19:13:16 +0300

    Add steps images.

--- components/front_page/Steps.vue
+++ components/front_page/Steps.vue
@@ -1,16 +1,28 @@
 <template>
   <section class="section grid grid-cols-1 sm:grid-cols-3 gap-4">
-    <StepCard step_num="1" text="Join Cyclic" />
-    <StepCard step_num="2" text="Connect GitHub" />
-    <StepCard step_num="3" text="Deploy" />
+    <StepCard :step_num="1" text="Start with what you know">
+      <ExampleCode />
+    </StepCard>
+
+    <StepCard :step_num="2" text="git push">
+      <div
+        class="text-center video_box"
+        style="max-height: 220px; overflow: hidden"
+      >
+        <img src="/images/deploy_tour.gif" >
+      </div>
+    </StepCard>
+
+    <StepCard :step_num="3" text="Go live" />
   </section>
 </template>
 
 <script>
+import ExampleCode from './ExampleCode.vue';
 import StepCard from './StepCard.vue';
 
   export default {
     name: 'Steps',
-    components: { StepCard }
+    components: { StepCard, ExampleCode }
 }
 </script>
