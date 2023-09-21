c7e0439951addddbbf5da1a8585dcf4dd09cdb9f
laweyez <allenojuka10@gmail.com>
2023-09-21 18:16:31 +0300

    Remove api integrations link.

--- components/Menu.vue
+++ components/Menu.vue
@@ -32,13 +32,13 @@
                   ChatOps: An Admin Bot
                 </nuxt-link>
 
-                <nuxt-link
+                <!-- <nuxt-link
                   class="capitalize text-neutral-300 hover:text-white px-3 py-2 hover:bg-neutral-700
                   rounded-lg transition w-full text-sm"
                   to="/use-cases/api-integrations"
                 >
                   API Integrations
-                </nuxt-link>
+                </nuxt-link> -->
 
                 <nuxt-link
                   class="capitalize text-neutral-300 hover:text-white px-3 py-2 hover:bg-neutral-700
@@ -194,13 +194,13 @@
               ChatOps: An Admin Bot
             </nuxt-link>
 
-            <nuxt-link
+            <!-- <nuxt-link
               class="capitalize text-neutral-300 hover:text-white px-3 py-2 hover:bg-neutral-700
               rounded-lg transition w-full text-sm"
               to="/use-cases/api-integrations"
             >
               API Integrations
-            </nuxt-link>
+            </nuxt-link> -->
 
             <nuxt-link
               class="capitalize text-neutral-300 hover:text-white px-3 py-2 hover:bg-neutral-700
