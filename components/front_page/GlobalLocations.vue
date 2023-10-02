bd81484c4669854b14d6970306f7381431bedf04
laweyez <allenojuka10@gmail.com>
2023-10-02 17:08:08 +0300

    GlobalLocations.

---
+++ components/front_page/GlobalLocations.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="section common-section">
+    <!-- ********* PILL TEXT ********* -->
+    <Pill text="Available everywhere" />
+    
+    <!-- ********* CTA TEXTS ********* -->
+    <div class="space-y-6">
+      <h2 class="font-extrabold text-4xl lg:text-5xl text-center">
+        Support users everywhere
+      </h2>
+  
+      <p class="text-center text-neutral-300">
+        Deploy your apps in multiple locations
+      </p>
+    </div>
+
+    <!-- ********* WORLD MAP ********* -->
+    <div class="">
+      <img class="w-full" src="/images/globe.svg" alt="Globe Locations">
+    </div>
+  </section>
+</template>
