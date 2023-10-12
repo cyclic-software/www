91d698f81c65bcb42961be19b97cbb424422705d
laweyez <allenojuka10@gmail.com>
2023-10-12 17:52:59 +0300

    Fix visibility.

--- components/cta/LaunchHelloWorld.vue
+++ components/cta/LaunchHelloWorld.vue
@@ -1,32 +1,22 @@
 <template>
-  <div class="px-4 sm:px-0">
-    <div class="relative w-full max-w-screen-xl h-80 mx-auto bg-[#245fff]
-    rounded-xl overflow-hidden">
-      <img
-        src="/images/Rectangle-240-copy.svg"
-        loading="lazy"
-        alt="Deploy on Cyclic"
-        class="absolute top-0 left-0"
+  <div class="relative w-full bg-neutral-800/20
+  border !border-neutral-700 rounded-xl flex flex-col items-center
+  gap-5 text-center p-8">
+      <h2 class="text-4xl font-bold">Launch a hello world</h2>
+
+      <p class="w-full md:w-[50%] text-neutral-300">
+        Fork and deploy a hello world project onto Cyclic faster than it has taken to read this page. Experience the power.
+      </p>
+
+      <a
+        href="https://app.cyclic.sh/api/login"
+        @click="sign_up('https://app.cyclic.sh/api/login?utm_source=www&utm_medium=web&utm_campaign=cta', $event)"
+        target="_blank"
+        class="bg-primary text-white capitalize flex items-center justify-center rounded-xl px-4 py-3
+        gap-2 hover:no-underline hover:scale-105 transition w-full md:w-max"
       >
-  
-      <div class="absolute w-full h-full z-[1] flex flex-col items-center
-      justify-center p-4 text-center">
-        <h2 class="text-4xl font-bold mb-3">Launch a hello world</h2>
-
-        <p class="text-lg w-full sm:w-[50%] mb-6">
-          Fork and deploy a hello world project onto Cyclic faster than it has taken to read this page. Experience the power.
-        </p>
-
-        <a
-          href="https://app.cyclic.sh/api/login"
-          @click="sign_up('https://app.cyclic.sh/api/login?utm_source=www&utm_medium=web&utm_campaign=cta', $event)"
-          target="_blank"
-          class="button white w-button"
-        >
-          Deploy on Cyclic 🚀
-        </a>
-      </div>
-    </div>
+        Deploy on Cyclic 🚀
+      </a>
   </div>
 </template>
 
