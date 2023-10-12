80ed90cb96b7b2a978bc74b450ad17045aac3b60
LawEyez <LawEyez@users.noreply.github.com>
2023-10-12 15:55:14 +0000

    deploy: 0a25a0c566e6b3f44a7d65c647164f1e7c866578

--- _nuxt/static/1697124164/use-cases/backends-for-frontends/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1697124164",layout:"default",error:a,serverRendered:true,routePath:"\u002Fuse-cases\u002Fbackends-for-frontends",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5a8f011e"}},globalRefs:{}}}(null));
---
+++ _nuxt/static/1697126086/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1697126086",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"61a343ac"}},globalRefs:{}}}(null,"\u002F"));
