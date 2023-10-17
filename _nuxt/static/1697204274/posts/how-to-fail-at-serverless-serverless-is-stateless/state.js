0b20b3a8a8f44060b7f789ef9c527da8769a1f6d
seekayel <seekayel@users.noreply.github.com>
2023-10-17 20:57:41 +0000

    deploy: 1b33747947588a44fb8332bf27678386638941a8

--- _nuxt/static/1697204274/posts/how-to-fail-at-serverless-serverless-is-stateless/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1697204274",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fhow-to-fail-at-serverless-serverless-is-stateless",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1502d8d1"}},globalRefs:{}}}(null));
---
+++ _nuxt/static/1697576235/posts/how-a-phishing-scampage-works/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1697576235",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fhow-a-phishing-scampage-works",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2d2bc18a"}},globalRefs:{}}}(null));
