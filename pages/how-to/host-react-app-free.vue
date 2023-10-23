3268ff5b5701c7f45214a89cc7a7e35ce96bb631
laweyez <allenojuka10@gmail.com>
2023-10-23 17:02:00 +0300

    Add react how to.

---
+++ pages/how-to/host-react-app-free.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class='section common-section'>
+    <div class="common-section">
+      <!-- ********* PILL TEXT ********* -->
+      <Pill text="React" />
+      
+      <!-- ********* CTA TEXTS ********* -->
+      <div class="space-y-6">
+        <h2 class="font-extrabold text-4xl lg:text-5xl text-center capitalize">
+          How to host a React app on Cyclic.
+        </h2>
+    
+        <p class="text-center text-neutral-300">
+          A quick guide to help you get up and running with React.
+        </p>
+      </div>
+    </div>
+
+    <div class='prose w-full md:max-w-screen-md mx-auto'>
+      
+    </div>
+  </section>
+</template>
