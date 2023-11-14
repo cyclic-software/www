232378ddccc9fe2a652379a26842278568ed52d4
Mike <korostelevm@gmail.com>
2023-11-14 12:54:46 -0500

    asdf

--- pages/pricing.vue
+++ pages/pricing.vue
@@ -349,7 +349,7 @@
 
           <div class="flex items-center gap-2">
             <i class="ri-checkbox-circle-fill text-primary"></i>
-            <p class="text-neutral-200">10 000 API requests / month</p>
+            <p class="text-neutral-200">1000 API requests / month</p>
           </div>
 
           <div class="flex items-center gap-2">
